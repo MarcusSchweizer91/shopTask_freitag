38f53cd3bd24e2434211c8fb3c3166fee92cf670
Marcus <marcus.schweizer91@gmail.com>
2022-11-04 15:32:26 +0100

    Add OrderRepo Class

--- src/main/java/org/shop/OrderRepo.java
+++ src/main/java/org/shop/OrderRepo.java
@@ -1,4 +1,33 @@
 package org.shop;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class OrderRepo {
+
+    //orders
+    List<Order>orderList = new ArrayList<>();
+
+    //Getter - list()
+    public List<Order> getOrderList(){
+        return orderList;
+    }
+
+    // GetOrderByID - get()
+    public Order getOrderByID(int orderID){
+        for (Order order: orderList){
+            if(order.getOrderID() == orderID){
+                return order;
+            }
+        }
+        return null;
+    }
+
+    //Add Order - add()
+    public void addOrder(Order order){
+        orderList.add(order);
+    }
+
+
+
 }
