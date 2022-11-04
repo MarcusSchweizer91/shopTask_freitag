2d1642a301ca5405d35e0ff5c9c939444d6b1d68
Marcus <marcus.schweizer91@gmail.com>
2022-11-04 14:29:40 +0100

    Add GetProductByName Add GetProductByID

--- src/main/java/org/shop/ProductRepo.java
+++ src/main/java/org/shop/ProductRepo.java
@@ -1,6 +1,31 @@
 package org.shop;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProductRepo {
 
+    List<Product> productList = new ArrayList<>();
 
+    public List<Product> getProductList() {
+        return productList;
+    }
+    // GetProductByName
+    public Product getProductByName(String productName){
+        for (Product product: productList) {
+            if (product.getName().equalsIgnoreCase(productName)){
+                return product;}
+        }
+        return null;
+    }
+    //GetProductByID
+    public Product getProductById(int productId){
+        for (Product product: productList) {
+            if (product.getId() == productId){
+                return product;
+            }
+
+        }
+        return null;
+    }
 }
