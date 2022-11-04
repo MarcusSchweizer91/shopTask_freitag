a7a2dd5aa1b606e9e68505d94443763631b8e982
Marcell <84642104+MoinMarcell@users.noreply.github.com>
2022-11-04 13:17:53 +0100

    initial commit branch marcell

--- src/main/java/org/shop/ProductRepo.java
+++ src/main/java/org/shop/ProductRepo.java
@@ -1,6 +1,35 @@
 package org.shop;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProductRepo {
+    List<Product> productList = new ArrayList<>();
 
+    public ProductRepo(List<Product> productList) {
+        this.productList = productList;
+    }
 
+    public ProductRepo() {}
+
+    public List<Product> getProductList() {
+        return productList;
+    }
+
+    public Product getProductByName(String name){
+        for(Product c : productList){
+            if(c.getName().toLowerCase().equals(name.toLowerCase())){
+                return c;
+            }
+        }
+        return null;
+    }
+    public Product getProductById(int id){
+        for(Product c : productList){
+            if(c.getId() == id){
+                return c;
+            }
+        }
+        return null;
+    }
 }
